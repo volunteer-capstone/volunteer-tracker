70ba0cba5922b8f5ce5c0a346833e30550ff9295
Brandi <brandiclinard021911@gmail.com>
2019-09-23 18:11:06 -0500

    added notes to functions to show what is what

--- src/main/java/com/codeup/volunteertracker/controllers/PositionController.java
+++ src/main/java/com/codeup/volunteertracker/controllers/PositionController.java
@@ -37,12 +37,14 @@ public class PositionController {
         return "redirect:/events/" + savePosition.getId();
     }
 
+//    UNTESTED -- EDIT
     @GetMapping("/events/positions/edit/{id}")
    public String editPosition(@PathVariable long id, Model viewModel){
         viewModel.addAttribute("position", positionDao.findOne(id));
         return "positions/edit";
     }
 
+//    UNTESTED -- EDIT
     @PostMapping("/events/positions/edit/{id}")
     public String editPosition(@PathVariable long id, @RequestParam(name="title") String title, @RequestParam(name="description") String description, @RequestParam(name="numNeeded") int numNeeded, @RequestParam(name="start") Date start, @RequestParam(name="end") Date end, Model viewModel) {
         Position editedPosition = positionDao.findOne(id);
@@ -56,6 +58,7 @@ public class PositionController {
         return "redirect:/events/" + eventId;
     }
 
+//    UNTESTED -- DELETE
     // unsure if this will work appropriately
     @GetMapping("/events/positions/delete/{id}")
     public String deletePosition(@PathVariable long id){
@@ -64,6 +67,7 @@ public class PositionController {
         return "redirect:/events/" + eventId;
     }
 
+//    UNTESTED -- DELETE
     @PostMapping("/events/positions/delete/{id}")
     public String afterDelete(@PathVariable long id){
         Position toDelete = positionDao.findOne(id);
