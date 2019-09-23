213bda8f3555fc83345953d8f5cfd4c53fca11fa
Brandi <brandiclinard021911@gmail.com>
2019-09-23 15:43:45 -0500

    working on position crud

--- src/main/java/com/codeup/volunteertracker/controllers/PositionController.java
+++ src/main/java/com/codeup/volunteertracker/controllers/PositionController.java
@@ -5,9 +5,7 @@ import com.codeup.volunteertracker.models.Position;
 import com.codeup.volunteertracker.repositories.EventRepository;
 import com.codeup.volunteertracker.repositories.PositionRepository;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 public class PositionController {
 
@@ -19,21 +17,33 @@ public class PositionController {
         this.eventDao = eventRepository;
     }
 
-//    UNTESTED
+//    UNTESTED-- create
     //will have to pass event id as a hidden attribute
     @GetMapping("/events/{id}/create-position")
-    public String createPosition(Model viewModel, long id){
+    public String createPosition(Model viewModel, @PathVariable long id){
         viewModel.addAttribute("position", new Position());
         Event event = eventDao.findOne(id);
         viewModel.addAttribute("event", event);
         return "/position/create";
     }
 
-    // UNTESTED
+    // UNTESTED-- create
     @PostMapping("/events/{id}/create-position")
     public String createPosition(@ModelAttribute Position position, @ModelAttribute Event event){
         position.setEvent(event);
         Position savePosition = positionDao.save(position);
         return "redirect:/events/{id}";
     }
+
+    @GetMapping("/events/positions/edit/{id}")
+   public String editPosition(@PathVariable long id, Model viewModel){
+        viewModel.addAttribute("position", positionDao.findOne(id));
+        return "positions/edit";
+    }
+
+
+//    @PostMapping("/events/positions/edit/{id}")
+//    public String editPosition(@PathVariable, long id, @RequestParam(name="title"), String title, @RequestParam(name="description") String description, @RequestParam(name="numNeeded") long numNeeded, @RequestParam(name="start") Datetime start, @RequestParam(name="end") DateTime end) {
+//
+//    }
 }
