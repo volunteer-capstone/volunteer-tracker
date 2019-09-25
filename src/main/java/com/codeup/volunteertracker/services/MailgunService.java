056777339d77264a1105cfbf320b3b2384687d98
Brandi <brandiclinard021911@gmail.com>
2019-09-25 16:30:25 -0500

    fixing password errors and hiding them

--- src/main/java/com/codeup/volunteertracker/services/MailgunService.java
+++ src/main/java/com/codeup/volunteertracker/services/MailgunService.java
@@ -2,6 +2,7 @@ package com.codeup.volunteertracker.services;
 
 import java.io.File;
 
+import com.codeup.volunteertracker.PasswordAccess;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class MailgunService {
+public class MailgunService extends PasswordAccess {
 
     @GetMapping("/email")
     public static String email(){
@@ -22,7 +23,7 @@ public class MailgunService {
     @PostMapping("/email")
     public static JsonNode sendSimpleMessage() throws UnirestException {
         HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/mg.pathofthevolunteer.com/messages")
-                .basicAuth("api", "PASSWORD")
+                .basicAuth("api", PASSWORD)
                 .field("from", "Tester for Path of the Volunteer <USER@YOURDOMAIN.COM>")
                 .field("to", "brandiclinard021911@gmail.com")
                 .field("subject", "Testing Mailgun email service")
