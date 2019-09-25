f7328c18ffaeea44af560b4df28b719c6e1332f2
Brandi <brandiclinard021911@gmail.com>
2019-09-25 14:37:50 -0500

    mailgun working. need to implement bulk sending

---
+++ src/main/java/com/codeup/volunteertracker/services/MailgunService.java
@@ -0,0 +1,35 @@
+package com.codeup.volunteertracker.services;
+
+import java.io.File;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MailgunService {
+
+    @GetMapping("/email")
+    public static String email(){
+
+        return "email";
+    }
+
+    @PostMapping("/email")
+    public static JsonNode sendSimpleMessage() throws UnirestException {
+        HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/mg.pathofthevolunteer.com/messages")
+                .basicAuth("api", "7a4eefc9b94e02c666fee2b50ff1199b-baa55c84-c14827be")
+                .field("from", "Excited User <USER@YOURDOMAIN.COM>")
+                .field("to", "brandiclinard021911@gmail.com")
+                .field("subject", "hello")
+                .field("text", "testing")
+                .asJson();
+
+        return request.getBody();
+
+    }
+}
