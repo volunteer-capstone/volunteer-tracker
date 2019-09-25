31fd92ac9bef7b26863a623d22a5adb7fc990e7f
Brandi <brandiclinard021911@gmail.com>
2019-09-25 14:51:24 -0500

    functioning controller for twilio text

--- src/main/java/com/codeup/volunteertracker/services/TwilioService.java
+++ src/main/java/com/codeup/volunteertracker/services/TwilioService.java
@@ -3,19 +3,33 @@ package com.codeup.volunteertracker.services;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class TwilioService {
     // Find your Account Sid and Token at twilio.com/user/account
-    public static final String ACCOUNT_SID = "SID FROM TWILIO ACCOUNT";
-    public static final String AUTH_TOKEN = "auth token";
+    public static final String ACCOUNT_SID = "AC4c785c0ddfbe4f35486902bcf9ed9a88";
+    public static final String AUTH_TOKEN = "ebc9ba8e0953e821460daf2f324b98b6";
 
-    public static void main(String[] args) {
+
+
+    @GetMapping("/text")
+    public String testingText(){
+        return "text";
+    }
+
+    @PostMapping("/text")
+    public String testing(){
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         Message message = Message.creator(new PhoneNumber("+12542319966"),
                 new PhoneNumber("+12543122613"),
-                "This is the ship that made the Kessel Run in fourteen parsecs?").create();
+                "This message is to test path of the volunteer twilio service.").create();
 
         System.out.println(message.getSid());
+
+        return "redirect:/events";
     }
 }
