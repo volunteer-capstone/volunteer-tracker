94cb283381537310e81ba73534b5d555ed61d0e1
Brandi <brandiclinard021911@gmail.com>
2019-09-25 16:31:18 -0500

    hidden password access

---
+++ src/main/java/com/codeup/volunteertracker/PasswordAccess.java
@@ -0,0 +1,8 @@
+package com.codeup.volunteertracker;
+
+public class PasswordAccess {
+
+    public static final String ACCOUNT_SID = "AC4c785c0ddfbe4f35486902bcf9ed9a88";
+    public static final String AUTH_TOKEN = "c3b9ade79202abc4867c3521dcc3d76a";
+    public static final String PASSWORD = "7a4eefc9b94e02c666fee2b50ff1199b-baa55c84-c14827be";
+}
