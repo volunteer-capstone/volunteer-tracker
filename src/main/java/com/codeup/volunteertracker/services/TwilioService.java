891c227b46edf2465e14eed3f070a37038472ec3
Brandi <brandiclinard021911@gmail.com>
2019-09-26 09:05:18 -0500

    again

--- src/main/java/com/codeup/volunteertracker/services/TwilioService.java
+++ src/main/java/com/codeup/volunteertracker/services/TwilioService.java
@@ -1,5 +1,6 @@
 package com.codeup.volunteertracker.services;
 
+import com.codeup.volunteertracker.Access;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class TwilioService {
+public class TwilioService extends Access {
     // Find your Account Sid and Token at twilio.com/user/account
 
 
