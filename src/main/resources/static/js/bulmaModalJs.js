72f5434faefc1712ef7902d2d421730805d3046b
Brandi <brandiclinard021911@gmail.com>
2019-10-01 15:04:24 -0500

    now it displays as a modal under the button

--- src/main/resources/static/js/bulmaModalJs.js
+++ src/main/resources/static/js/bulmaModalJs.js
@@ -23,19 +23,19 @@
             html.classList.remove('is-clipped');
         });
     });
-    document.querySelectorAll('a#openSMSModal').addEventListener('click', function(event) {
+    document.querySelectorAll('a#openModal2').addEventListener('click', function(event) {
         event.preventDefault();
-        let modal = document.querySelector('.modal');  // assuming you have only 1
+        let modal = document.querySelector('.modal2');  // assuming you have only 1
         let html = document.querySelector('html');
         modal.classList.add('is-active');
         html.classList.add('is-clipped');
 
-        modal.querySelector('.modal-background').addEventListener('click', function(e) {
+        modal.querySelector('.modal-background2').addEventListener('click', function(e) {
             e.preventDefault();
             modal.classList.remove('is-active');
             html.classList.remove('is-clipped');
         });
-        modal.querySelector('.modal-close').addEventListener('click', function(e) {
+        modal.querySelector('.modal-close2').addEventListener('click', function(e) {
             e.preventDefault();
             modal.classList.remove('is-active');
             html.classList.remove('is-clipped');
