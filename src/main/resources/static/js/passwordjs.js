8851f50dd442719669f4d51e19f3c1a651cd9cf4
Nicholas Sosa <nicksosa456@gmail.com>
2019-10-02 18:23:20 -0500

    phone number validation works

--- src/main/resources/static/js/passwordjs.js
+++ src/main/resources/static/js/passwordjs.js
@@ -1,10 +1,11 @@
 "use strict";
 let passwordConfirm = document.querySelector('#passwordConfirm');
 let password = document.querySelector('#password');
+let phoneNum = document.querySelector('#phoneNumber');
 
 passwordConfirm.addEventListener('keyup', function () {passCheck()});
 password.addEventListener('keyup', function () {passCheck()});
-
+phoneNum.addEventListener('blur', function(){phoneNumber()});
 
 function passCheck() {
     let password = document.querySelector('#password').value;
@@ -29,5 +30,15 @@ function passCheck() {
         document.querySelector('#register').style.display = "inline";
         document.querySelector('#editDisabled').style.display = "none";
         document.querySelector('#edit').style.display = "inline";
+    }
+}
+
+function phoneNumber() {
+    let number = document.querySelector('#phoneNumber').value;
+    let phoneNo = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+    if(number.match(phoneNo)) {
+        document.querySelector('#phoneNumber2').style.display="none";
+    } else {
+        document.querySelector('#phoneNumber2').style.display="inline";
     }
 }
