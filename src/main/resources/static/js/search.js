2df51d7bb13c7a2176181d7ce79fa75096d40cb7
Austin Ghigliotty <austin.ghigliotty@gmail.com>
2019-10-03 14:13:30 -0500

    event search now case insensitive

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -4,18 +4,18 @@ var posInput = '';
 
 function searchEvents() {
     var eventList = $(".event");
-    input = $('#eventSearch').val();
+    input = $('#eventSearch').val().toUpperCase();
 
     let posCheck = false;
     for (let i = 0; eventList.length > i; i++) {
         posCheck = false;
         var posList = $(eventList[i]).find(".positionDiv");
-        if ($(eventList[i]).children().attr("class").indexOf(input) > -1) {
+        if ($(eventList[i]).children().attr("class").toUpperCase().indexOf(input) > -1) {
             posCheck = true;
         } else {
             if ($('#posCheckbox').is(':checked')) {
                 for (let j = 0; posList.length > j; j++) {
-                    if ($(posList[j]).children().text().indexOf(input) > -1) {
+                    if ($(posList[j]).children().text().toUpperCase().indexOf(input) > -1) {
                         posCheck = true;
                     }
                 }
