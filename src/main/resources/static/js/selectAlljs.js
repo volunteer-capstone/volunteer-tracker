a6840e75ee6d9e9c402610b3bdc307e673a9c5e6
Nicholas Sosa <nicksosa456@gmail.com>
2019-10-07 11:46:52 -0500

    fixed check all

--- src/main/resources/static/js/selectAlljs.js
+++ src/main/resources/static/js/selectAlljs.js
@@ -1,24 +1,20 @@
 "use strict";
-(function(){
-    $(document).ready(function(){
-        function checkAll(source) {
-            let checkboxes = document.getElementsByName('check');
-            for(let i=0; i < checkboxes.length ; i++) {
-                checkboxes[i].checked = source.checked;
-            }
-        }
+function checkAll(source) {
+    let checkboxes = document.getElementsByName('check');
+    for(let i=0; i < checkboxes.length ; i++) {
+        checkboxes[i].checked = source.checked;
+    }
+}
 
-        function getAll(selector) {
-            return Array.prototype.slice.call(document.querySelectorAll(selector), 0)
-        }
+function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0)
+}
 
-        let volunteerBtns = getAll(".changeBtn");
-        if (volunteerBtns.length > 0) {
-            volunteerBtns.forEach(function (el, i) {
-                el.addEventListener('click', function () {
-                    el.classList.add('is-loading');
-                })
-            })
-        }
+let volunteerBtns = getAll(".changeBtn");
+if (volunteerBtns.length > 0) {
+    volunteerBtns.forEach(function (el, i) {
+        el.addEventListener('click', function () {
+            el.classList.add('is-loading');
+        })
     })
-})();
+}
